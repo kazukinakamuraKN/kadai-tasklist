e5fe1be73d1dc477fdc3c1b4c8d2341e9991a753
kazuki.nakamura <kajirikajiri@gmail.com>
2018-04-19 03:31:08 +0900

    Add validation

---
+++ src/controllers/CreateServlet.java
@@ -0,0 +1,53 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class CreateServlet
+ */
+@WebServlet("/create")
+public class CreateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EntityManager em = DBUtil.createEntityManager();
+		em.getTransaction().begin();
+
+		Task t = new Task();
+
+		String content = request.getParameter("content");
+		t.setContent(content);
+
+		String _token = (String)request.getParameter("_token");
+		if(_token != null && _token.equals(request.getSession().getId())){
+			em.persist(t);
+			em.getTransaction().commit();
+		}
+
+		em.close();
+
+		response.sendRedirect(request.getContextPath() + "/index");
+	}
+
+}
